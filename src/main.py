0abd82d69b85a55eed73d6be1312b8dfc3b03a0b
Yevhen <yevgenphk@gmail.com>
2025-03-25 17:32:11 +0200

    Fixes

--- src/main.py
+++ src/main.py
@@ -1,6 +1,4 @@
 import flet as ft
-from flet.core.text_style import TextThemeStyle
-import logging
 
 
 def main(page: ft.Page):
@@ -11,7 +9,7 @@ def main(page: ft.Page):
     # Placeholder for the main content that will update based on navigation
     content = ft.Column(
         controls=[
-            ft.Text("Welcome to My Portfolio!", style=TextThemeStyle.HEADLINE_MEDIUM),
+            ft.Text("Welcome to My Portfolio!", style=ft.TextThemeStyle.HEADLINE_MEDIUM),
             ft.Text("This is the homepage of your portfolio website.")
         ],
         expand=True,
@@ -22,22 +20,22 @@ def main(page: ft.Page):
         # Use the data property to identify which navigation option was clicked
         if e.control.selected_index == 0:
             content.controls = [
-                ft.Text("Welcome to My Portfolio!", style=TextThemeStyle.HEADLINE_MEDIUM),
+                ft.Text("Welcome to My Portfolio!", style=ft.TextThemeStyle.HEADLINE_MEDIUM),
                 ft.Text("This is the homepage of your portfolio website.")
             ]
         elif e.control.selected_index == 1:
             content.controls = [
-                ft.Text("About Me", style=TextThemeStyle.HEADLINE_MEDIUM),
+                ft.Text("About Me", style=ft.TextThemeStyle.HEADLINE_MEDIUM),
                 ft.Text("A brief introduction about yourself, your background, and skills.")
             ]
         elif e.control.selected_index == 2:
             content.controls = [
-                ft.Text("Projects", style=TextThemeStyle.HEADLINE_MEDIUM),
+                ft.Text("Projects", style=ft.TextThemeStyle.HEADLINE_MEDIUM),
                 ft.Text("List or showcase your projects here with images, descriptions, and links.")
             ]
         elif e.control.selected_index == 3:
             content.controls = [
-                ft.Text("Contact",  style=TextThemeStyle.HEADLINE_MEDIUM),
+                ft.Text("Contact",  style=ft.TextThemeStyle.HEADLINE_MEDIUM),
                 ft.Text("Include your contact information or a contact form here.")
             ]
         # Refresh the content area
